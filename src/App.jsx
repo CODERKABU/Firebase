8e6b68dedc1b2df51b3aa03a29a2a454604f9a62
CODERKABU <159780305+CODERKABU@users.noreply.github.com>
2025-03-25 16:03:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import {ToastContainer, toast} from "react-toastify";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [record , setRecord] =useState({});
+  const [record, setRecord] = useState({});
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     getUserData();
@@ -32,13 +33,23 @@ function App() {
 
   let submitData = async (e) => {
     e.preventDefault();
-    await addDoc(getUsers, record);
+    if(index!=0){
+      let getUserData = doc(db,"users", index);
+      await updateDoc(getUserData, record);
+      toast.success("Data Edited Successfully!!",{
+        position:"bottom-right",
+        autoClose:2500
+      })
+    }else{
+      await addDoc(getUsers, record); 
+      toast.success("Data Added Successfully!!",{
+        position:"bottom-right",
+        autoClose:2500
+      })
+    }
     getUserData();
     setRecord({});
-    toast.success("Data Added Successfully!!",{
-      position:"bottom-right",
-      autoClose:2500
-    })
+    setIndex(0);
   }
 
   let deleteUser = async(id) => {
@@ -49,6 +60,15 @@ function App() {
       position:"bottom-right",
       autoClose:2500
     })
+  }
+
+  let updateUser = async (id) =>{
+    setIndex(id);
+    let editDoc = await doc(db,"users",id);
+    let singleUser = await getDoc(editDoc);
+      setRecord(singleUser.data());
+    
+    
   }
 
 
@@ -72,9 +92,27 @@ function App() {
           </tr>
 
           <tr>
+            <td>Password</td>
+            <td>
+              <input type="text" name="password" onChange={(e) => getInput(e)} value={record.password?record.password:""}/>
+            </td>
+          </tr>
+
+          <tr>
+            <td>gender</td>
+            <td>
+              <input type="radio" name="male" value="male" onChange={(e) => getInput(e)} checked={record.gender=='male'?"checked":""} /> Male
+              <input type="radio" name="female" value="female" onChange={(e) => getInput(e)} checked={record.gender == 'female'?"checked":""} /> Female
+            </td>            
+
+
+            
+          </tr>
+
+          <tr>
             <td></td>
             <td>
-              <input type="submit" />
+              <input type="submit" name="submit" value={index?"edit": "submit"} />
             </td>
           </tr>
         </table>
@@ -90,6 +128,8 @@ function App() {
             <th>NO</th>
             <th>Name</th>
             <th>Email</th>
+            <th>Password</th>
+            <th>Gender</th>
             <th>Action</th>
           </tr>
         </thead>
@@ -99,10 +139,12 @@ function App() {
               <td>{++i}</td>
               <td>{u.name}</td>
               <td>{u.email}</td>
+              <td>{u.password}</td>
+              <td>{u.gender}</td>
               <td>
-                <button onClick={()=> deleteUser(u.id)}>Del</button>
+                <button onClick={() => deleteUser(u.id)}>Del</button>
                 ||
-                <button>Edit</button>
+                <button onClick={() => updateUser(u.id)}>Edit</button>
               </td>
             </tr>
           ))}
